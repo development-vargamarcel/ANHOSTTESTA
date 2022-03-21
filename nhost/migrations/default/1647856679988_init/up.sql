a9af5d6dde6b93a1ce4d3433e947c24e6417a19d
development-vargamarcel <development.vargamarcel@gmail.com>
2022-03-21 11:22:19 +0100

    xx

--- nhost/migrations/default/1647856679988_init/up.sql
+++ nhost/migrations/default/1647856679988_init/up.sql
@@ -1,16 +1,16 @@
 SET check_function_bodies = false;
-CREATE TABLE public.ww (
+CREATE TABLE public.xx (
     id integer NOT NULL,
     created_at timestamp with time zone DEFAULT now() NOT NULL
 );
-CREATE SEQUENCE public.ww_id_seq
+CREATE SEQUENCE public.xx_id_seq
     AS integer
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
     NO MAXVALUE
     CACHE 1;
-ALTER SEQUENCE public.ww_id_seq OWNED BY public.ww.id;
-ALTER TABLE ONLY public.ww ALTER COLUMN id SET DEFAULT nextval('public.ww_id_seq'::regclass);
-ALTER TABLE ONLY public.ww
-    ADD CONSTRAINT ww_pkey PRIMARY KEY (id);
+ALTER SEQUENCE public.xx_id_seq OWNED BY public.xx.id;
+ALTER TABLE ONLY public.xx ALTER COLUMN id SET DEFAULT nextval('public.xx_id_seq'::regclass);
+ALTER TABLE ONLY public.xx
+    ADD CONSTRAINT xx_pkey PRIMARY KEY (id);
