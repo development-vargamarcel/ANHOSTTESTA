15aad295a634c4283b87cfff03892574fb6f8ede
development-vargamarcel <development.vargamarcel@gmail.com>
2022-03-21 11:08:25 +0100

    bb

--- nhost/migrations/default/1647856679988_init/up.sql
+++ nhost/migrations/default/1647856679988_init/up.sql
@@ -1,16 +1,16 @@
 SET check_function_bodies = false;
-CREATE TABLE public.aa (
+CREATE TABLE public.bb (
     id integer NOT NULL,
     created_at timestamp with time zone DEFAULT now() NOT NULL
 );
-CREATE SEQUENCE public.aa_id_seq
+CREATE SEQUENCE public.bb_id_seq
     AS integer
     START WITH 1
     INCREMENT BY 1
     NO MINVALUE
     NO MAXVALUE
     CACHE 1;
-ALTER SEQUENCE public.aa_id_seq OWNED BY public.aa.id;
-ALTER TABLE ONLY public.aa ALTER COLUMN id SET DEFAULT nextval('public.aa_id_seq'::regclass);
-ALTER TABLE ONLY public.aa
-    ADD CONSTRAINT aa_pkey PRIMARY KEY (id);
+ALTER SEQUENCE public.bb_id_seq OWNED BY public.bb.id;
+ALTER TABLE ONLY public.bb ALTER COLUMN id SET DEFAULT nextval('public.bb_id_seq'::regclass);
+ALTER TABLE ONLY public.bb
+    ADD CONSTRAINT bb_pkey PRIMARY KEY (id);
